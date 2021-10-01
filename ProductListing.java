3d8d7355e5a5585d2a3673125909113a7b733b3b
M.Yusup <91756995+muhamadyusup111120@users.noreply.github.com>
2021-10-02 03:26:56 +0700

    Add files via upload

---
+++ ProductListing.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UTS;
+
+/**
+ *
+ * @author M YUSUP
+ */
+public class ProductListing {
+    public static void main(String[] args) {
+        Product product_a = new Product("product a");
+        product_a.getProductListing(5, 15, 10);
+    }
+}
